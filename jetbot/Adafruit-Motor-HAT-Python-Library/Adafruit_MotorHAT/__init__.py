347abaa6489519a47cb735193cf9e37606edb0c6
JoshSYoon <46247601+JoshSYoon@users.noreply.github.com>
2021-08-16 08:59:22 -0700

    Create __init__.py

---
+++ jetbot/Adafruit-Motor-HAT-Python-Library/Adafruit_MotorHAT/__init__.py
@@ -0,0 +1,16 @@
+# Python library for interfacing with the Adafruit Motor HAT for Raspberry Pi to control DC motors with speed control
+# and Stepper motors with single, double, interleave and microstepping.
+
+# Designed specifically to work with the Adafruit Motor Hat
+
+# ----> https://www.adafruit.com/product/2348
+
+# Adafruit invests time and resources providing this open source code, please support Adafruit and open-source
+# hardware by purchasing products from Adafruit!
+
+# Written by Limor Fried for Adafruit Industries. MIT license. 
+
+# Modified by Joshua Yoon to work with GPIO instead of I2C
+
+# Got rid of importing the stepper motor class
+from Adafruit_MotorHAT.Adafruit_MotorHAT_Motors import Adafruit_DCMotor, Adafruit_MotorHAT
